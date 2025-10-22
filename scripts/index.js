31569852650884dfe5c8e7a7ba4895ea3942fc07
HABAB0 <tolya.shaprove@mail.ru>
2025-10-22 16:33:52 +0700

    add file format check

--- scripts/index.js
+++ scripts/index.js
@@ -5,123 +5,130 @@ const jsonUploadField = document.getElementById("json-upload-field");
 const resetButton = document.getElementById("button--reset");
 const mainContainer = document.getElementById("file-upload");
 
+
+
 document.getElementById("json-file").addEventListener ("change" ,(event) => {
     const file = event.target.files[0]
-    const reader = new FileReader();
-    reader.onload = function(e) {
-        data = JSON.parse(e.target.result);
-        jsonUploadField.style.display = "none";
-        resetButton.style.display = "block";
-        mainContainer.style.backgroundImage = "none";
-        formWrapper.style.display = "block";
+    if (file.name.endsWith(".json") || file.name.endsWith(".js") || file.name.endsWith(".JSON")){
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            data = JSON.parse(e.target.result);
+            jsonUploadField.style.display = "none";
+            resetButton.style.display = "block";
+            mainContainer.style.backgroundImage = "none";
+            formWrapper.style.display = "block";
 
-        if(data.fields){
-            data.fields.forEach(element => {
-                const label = document.createElement("label");
-                label.classList.add('file-upload__form--label');
-                let input;
+            if(data.fields){
+                data.fields.forEach(element => {
+                    const label = document.createElement("label");
+                    label.classList.add('file-upload__form--label');
+                    let input;
 
-                if (element.label) {
-                    label.textContent = element.label;
-                }
-
-                if (Object.values(element.input).some(value => Array.isArray(value))){
-                    if (Array.isArray(element.input.filetype)) {
-                        input = document.createElement("input");
-                        input.type = "file";
-                        input.multiple = element.input.multiple === true;
-
-                        const accept = element.input.filetype
-                            .map(item => item.startsWith('.') ? item : '.' + item)
-                            .join(',');
-                        input.accept = accept;
-
-                        input.classList.add('file-upload__form--input');
-                    } else {
-                    input = document.createElement("select");
-                    input.classList.add('file-upload__form--select')
-                    if (element.input.multiple === true) {
-                        input.multiple = true;
+                    if (element.label) {
+                        label.textContent = element.label;
                     }
-                    let values = [];
-                        for (const key in element.input) {
-                            if (Array.isArray(element.input[key])) {
-                                values = element.input[key];
-                                break;
-                            }
+
+                    if (Object.values(element.input).some(value => Array.isArray(value))){
+                        if (Array.isArray(element.input.filetype)) {
+                            input = document.createElement("input");
+                            input.type = "file";
+                            input.multiple = element.input.multiple === true;
+
+                            const accept = element.input.filetype
+                                .map(item => item.startsWith('.') ? item : '.' + item)
+                                .join(',');
+                            input.accept = accept;
+
+                            input.classList.add('file-upload__form--input');
+                        } else {
+                        input = document.createElement("select");
+                        input.classList.add('file-upload__form--select')
+                        if (element.input.multiple === true) {
+                            input.multiple = true;
                         }
-                        values.forEach(item => {
-                            const option = document.createElement("option");
-                            option.classList.add('file-upload__form--option')
-                            option.value = item;
-                            option.textContent = item;
-                            option.style.backgroundColor = item;
-                            input.appendChild(option);
-                        });
+                        let values = [];
+                            for (const key in element.input) {
+                                if (Array.isArray(element.input[key])) {
+                                    values = element.input[key];
+                                    break;
+                                }
+                            }
+                            values.forEach(item => {
+                                const option = document.createElement("option");
+                                option.classList.add('file-upload__form--option')
+                                option.value = item;
+                                option.textContent = item;
+                                option.style.backgroundColor = item;
+                                input.appendChild(option);
+                            });
+                        }
+                    }else {
+                        input = document.createElement("input");
+                        input.type = element.input.type; 
                     }
-                }else {
-                    input = document.createElement("input");
-                    input.type = element.input.type; 
-                }
 
-                if (element.input.required === true) {
-                    input.required = true;
-                }
-                if (element.input.mask) {
-                    input.placeholder = element.input.mask;
-                }
-                if (element.input.placeholder) {
-                    input.placeholder = element.input.placeholder;
-                }
-                input.classList.add('file-upload__form--input')
-                label.appendChild(input);
-                form.appendChild(label);
-            });
-        }
-
-        if(data.references){
-            const label = document.createElement("label");
-            label.classList.add('file-upload__label--references');
-            data.references.forEach(element => {
-                if(element.input){
-                    let input = document.createElement("input");
-                    input.type = element.input.type; 
                     if (element.input.required === true) {
                         input.required = true;
                     }
-                    if (element.input.checked === "true") {
-                        input.checked = true;
+                    if (element.input.mask) {
+                        input.placeholder = element.input.mask;
                     }
+                    if (element.input.placeholder) {
+                        input.placeholder = element.input.placeholder;
+                    }
+                    input.classList.add('file-upload__form--input')
                     label.appendChild(input);
-                } else if (element["text without ref"] || element.text || element.ref){
-                    let refText = document.createElement("p");
-                    if (element["text without ref"]){
-                        refText.appendChild(document.createTextNode(element["text without ref"] + " "));
-                    }
-                    let link = document.createElement("a");
-                    link.appendChild(document.createTextNode(element.text));
-                    link.href = "#" + element.ref; 
-                    refText.appendChild(link);
-                    label.appendChild(refText);
-                }
-                form.appendChild(label);
-            })
-        }
+                    form.appendChild(label);
+                });
+            }
 
-        if(data.buttons){
-            data.buttons.forEach(element => {
-                if(element.text){
-                    let button = document.createElement("button");
-                    button.classList.add('file-upload__form--button')
-                    button.textContent = element.text;
-                    button.type = "submit"
-                    form.appendChild(button);
-                }
-            })
+            if(data.references){
+                const label = document.createElement("label");
+                label.classList.add('file-upload__label--references');
+                data.references.forEach(element => {
+                    if(element.input){
+                        let input = document.createElement("input");
+                        input.type = element.input.type; 
+                        if (element.input.required === true) {
+                            input.required = true;
+                        }
+                        if (element.input.checked === "true") {
+                            input.checked = true;
+                        }
+                        label.appendChild(input);
+                    } else if (element["text without ref"] || element.text || element.ref){
+                        let refText = document.createElement("p");
+                        if (element["text without ref"]){
+                            refText.appendChild(document.createTextNode(element["text without ref"] + " "));
+                        }
+                        let link = document.createElement("a");
+                        link.appendChild(document.createTextNode(element.text));
+                        link.href = "#" + element.ref; 
+                        refText.appendChild(link);
+                        label.appendChild(refText);
+                    }
+                    form.appendChild(label);
+                })
+            }
+
+            if(data.buttons){
+                data.buttons.forEach(element => {
+                    if(element.text){
+                        let button = document.createElement("button");
+                        button.classList.add('file-upload__form--button')
+                        button.textContent = element.text;
+                        button.type = "submit"
+                        form.appendChild(button);
+                    }
+                })
+            }
+            
         }
-        
+        reader.readAsText(file);
+    } else {
+        event.target.value = ""
+        alert("Фаил не фаил .JSON")
     }
-    reader.readAsText(file);
 })
 
 if (resetButton && form) {

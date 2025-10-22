e2e82d0c829748e40eed73198bf338645c745378
HABAB0 <tolya.shaprove@mail.ru>
2025-10-22 16:19:23 +0700

    add styles for form

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,24 @@
 const form = document.getElementById("forms-container");
+const formWrapper = document.getElementById("forms-wrapper");
+formWrapper.style.display = "none";
+const jsonUploadField = document.getElementById("json-upload-field");
+const resetButton = document.getElementById("button--reset");
+const mainContainer = document.getElementById("file-upload");
+
 document.getElementById("json-file").addEventListener ("change" ,(event) => {
     const file = event.target.files[0]
-
     const reader = new FileReader();
     reader.onload = function(e) {
         data = JSON.parse(e.target.result);
-        console.log(data)
+        jsonUploadField.style.display = "none";
+        resetButton.style.display = "block";
+        mainContainer.style.backgroundImage = "none";
+        formWrapper.style.display = "block";
 
         if(data.fields){
             data.fields.forEach(element => {
                 const label = document.createElement("label");
-                label.classList.add('file-upload__label');
+                label.classList.add('file-upload__form--label');
                 let input;
 
                 if (element.label) {
@@ -18,7 +26,20 @@ document.getElementById("json-file").addEventListener ("change" ,(event) => {
                 }
 
                 if (Object.values(element.input).some(value => Array.isArray(value))){
+                    if (Array.isArray(element.input.filetype)) {
+                        input = document.createElement("input");
+                        input.type = "file";
+                        input.multiple = element.input.multiple === true;
+
+                        const accept = element.input.filetype
+                            .map(item => item.startsWith('.') ? item : '.' + item)
+                            .join(',');
+                        input.accept = accept;
+
+                        input.classList.add('file-upload__form--input');
+                    } else {
                     input = document.createElement("select");
+                    input.classList.add('file-upload__form--select')
                     if (element.input.multiple === true) {
                         input.multiple = true;
                     }
@@ -31,11 +52,13 @@ document.getElementById("json-file").addEventListener ("change" ,(event) => {
                         }
                         values.forEach(item => {
                             const option = document.createElement("option");
-                            option.classList.add('file-upload__option')
+                            option.classList.add('file-upload__form--option')
                             option.value = item;
                             option.textContent = item;
+                            option.style.backgroundColor = item;
                             input.appendChild(option);
                         });
+                    }
                 }else {
                     input = document.createElement("input");
                     input.type = element.input.type; 
@@ -50,18 +73,15 @@ document.getElementById("json-file").addEventListener ("change" ,(event) => {
                 if (element.input.placeholder) {
                     input.placeholder = element.input.placeholder;
                 }
-                if (element.input.filetype) {
-                    input.filetype = element.input.filetype
-                        .map(item => item.startsWith('.') ? item : '.' + item)
-                        .join(',');
-                }
-                input.classList.add('file-upload__input')
+                input.classList.add('file-upload__form--input')
                 label.appendChild(input);
                 form.appendChild(label);
             });
         }
 
         if(data.references){
+            const label = document.createElement("label");
+            label.classList.add('file-upload__label--references');
             data.references.forEach(element => {
                 if(element.input){
                     let input = document.createElement("input");
@@ -69,16 +89,22 @@ document.getElementById("json-file").addEventListener ("change" ,(event) => {
                     if (element.input.required === true) {
                         input.required = true;
                     }
-                    form.appendChild(input)
+                    if (element.input.checked === "true") {
+                        input.checked = true;
+                    }
+                    label.appendChild(input);
                 } else if (element["text without ref"] || element.text || element.ref){
                     let refText = document.createElement("p");
-                    refText.appendChild(document.createTextNode(element["text without ref"] + " "));
+                    if (element["text without ref"]){
+                        refText.appendChild(document.createTextNode(element["text without ref"] + " "));
+                    }
                     let link = document.createElement("a");
                     link.appendChild(document.createTextNode(element.text));
                     link.href = "#" + element.ref; 
                     refText.appendChild(link);
-                    form.appendChild(refText);
+                    label.appendChild(refText);
                 }
+                form.appendChild(label);
             })
         }
 
@@ -86,7 +112,9 @@ document.getElementById("json-file").addEventListener ("change" ,(event) => {
             data.buttons.forEach(element => {
                 if(element.text){
                     let button = document.createElement("button");
+                    button.classList.add('file-upload__form--button')
                     button.textContent = element.text;
+                    button.type = "submit"
                     form.appendChild(button);
                 }
             })
@@ -96,9 +124,16 @@ document.getElementById("json-file").addEventListener ("change" ,(event) => {
     reader.readAsText(file);
 })
 
-const resetButton = document.getElementById("button--reset");
 if (resetButton && form) {
     resetButton.addEventListener("click", function() {
-        form.innerHTML = "";
+        form.classList.add("forms-crumpling");
+        setTimeout(() => {
+            jsonUploadField.style.display = "block";
+            resetButton.style.display = "none";
+            mainContainer.style.backgroundImage = "url(../img/button-background.png)";
+            formWrapper.style.display = "none";
+            form.innerHTML = "";
+            form.classList.remove("forms-crumpling");
+        }, 700);
     });
 }
